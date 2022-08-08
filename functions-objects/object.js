16ba13ddf3003cfb393f991656963168f9bed369
niemand20 <zahid17290@gmail.com>
2022-08-08 13:52:53 +0100

    object

---
+++ functions-objects/object.js
@@ -0,0 +1,27 @@
+var student = {
+     id:115,
+     name:"Solaiman Khan", 
+     class: 9, 
+     marks: 98
+    
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '128gb',
+    camera: '7MP',
+
+}
+var myComputer = {
+    brand: 'hp',
+    price: 50000,
+    color: 'silver',
+    processor: 'i7'
+}
+
+console.log(myComputer);
+myComputer.price = 90000;
+console.log(myComputer);
+
+console.log(mobile.brand);
